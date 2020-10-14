e79ea0b8eda0784b2325a675ebefde8d9d352347
Marshall88GB <gasic.boban@gmail.com>
2020-10-14 22:32:27 +0200

    Updates

---
+++ precache-manifest.14ae5f179fe99868c552796a16b80052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb250c0e0b4d12d36159e1350aca27b",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "360a53bfb9752a79471e",
+    "url": "/Monster-Rolodex/static/css/main.e6d84f69.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360a53bfb9752a79471e",
+    "url": "/Monster-Rolodex/static/js/main.d3449b04.chunk.js"
+  },
+  {
+    "revision": "2b55c592b5ec82aad6ec",
+    "url": "/Monster-Rolodex/static/js/runtime-main.28f6930b.js"
+  }
+]);
